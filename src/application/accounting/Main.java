ae06f38a19d1ea1ba0f4e962b992585c17502a20
Tim <t.erdweg@fz-juelich.de>
2015-10-19 16:58:10 +0200

    Daten nun von der Kommandozeile

--- src/application/accounting/Main.java
+++ src/application/accounting/Main.java
@@ -2,6 +2,14 @@ package application.accounting;
 
 public class Main{
 	public static void main(String []args){
-		Buchhaltung.call_main(args);
+		if (args.length==0){
+			Buchhaltung.call_main(args);
+		} else {
+			ArgParser ap = new ArgParser(args);
+			args[0] = ap.getInputFilename();
+			args[1] = ap.getNonOptions();
+			args[2] = ap.getOutputFilename();
+			Buchhaltung.call_main(args);
+		}
 	}
 }
