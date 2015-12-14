e998934109f7b88f8f182f2335f5e2705f546474
Tim <t.erdweg@fz-juelich.de>
2015-12-14 16:59:27 +0100

    nur sparer geaendert  noch nicht vollstaendig

--- src/application/accounting/Sparer.java
+++ src/application/accounting/Sparer.java
@@ -7,12 +7,12 @@ public class Sparer {
 	private int mitgliedsnummer;
 	private String name;
 	private String vorname;
-	private double vorjahr;
-	private Map<Integer, Double> zahlenpaare = new HashMap<>();
+	private BigDecimal vorjahr;
+	private Map<Integer, BigDecimal> zahlenpaare = new HashMap<>();
 	private List<Integer> tage = new LinkedList<>();
-	private double p;
+	private BigDecimal p;
 	
-	public Sparer(int mitgliedsnummer, String name, String vorname, double vorjahr, double p){
+	public Sparer(int mitgliedsnummer, String name, String vorname, BigDecimal vorjahr, BigDecimal p){
 		this.mitgliedsnummer = mitgliedsnummer;
 		this.name = name;
 		this.vorname = vorname;
@@ -20,7 +20,7 @@ public class Sparer {
 		this.p = p;
 	}
 	
-	public void setZahlenpaar(int tag, double wert){
+	public void setZahlenpaar(int tag, BigDecimal wert){
 		this.zahlenpaare.put(tag, wert);
 		this.tage.add(tag);
 	}
@@ -37,20 +37,26 @@ public class Sparer {
 		return this.vorname;
 	}
 	
-	public double getGesamt(double p){
-		double wert = this.vorjahr*(this.p/(100)+1);
+	public BigDecimal getGesamt(BigDecimal p){
+		BigDecimal wert = new BigDecimal(this.vorjahr.mult(this.p.divide(100).add(1)));
 		int i = 0;
 		try{
 			while (true){
-				double plus = this.zahlenpaare.get(this.tage.get(i));
-				wert += (plus*(this.p/(100)*(360-this.tage.get(i))/(360)+1));
+				BigDecimal plus = new BigDecimal(this.zahlenpaare.get(this.tage.get(i)));
+				//wert += (plus*(this.p/(100)*(360-this.tage.get(i))/(360)+1));
+				wert.add(this.interestAmount(plus, this.p, this.tage.get(i)));
 				i++;
 			}
 		} catch (IndexOutOfBoundsException e){}
-		wert *= 100;
-		wert = Math.round(wert);
+		wert = wert.mult(100);
+		//wert = Math.round(wert);
+		/////wert.setScale(0, BigDecimal.ROUND_HALF_UP).toString();
 		wert /= 100;
 		return wert;
+	}
+	
+	BigDecimal interestAmount(BigDecimal plus, BigDecimal p, int tage){
+		return (plus.mult(p.divide(100).mult(360-tage).divide(360).add(1)));
 	}
 	
 }
